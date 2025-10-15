2d4f316c8ef1612df3c8ef49259f757957b1cbe6
anaiva <a.ivanchenko.eu@gmail.com>
2025-10-15 14:14:36 +0300

    add comments

--- Farm/FarmService.cs
+++ Farm/FarmService.cs
@@ -5,9 +5,21 @@ using Grpc.Core;
 
 public class FarmService : Services.Farm.FarmBase
 {
-    // NOTE: replace this stub logic with your real FarmLogic calls
+    /// <summary>
+    /// Initializes a new instance of FarmLogic
+    /// </summary>
     private readonly FarmLogic mLogic = new FarmLogic();
 
+
+    /// <summary>
+    /// Submits a specified amount of food to the farm for processing.
+    /// </summary>
+    /// <param name="request">The gRPC request message with the amount of food to submit.</param>
+    /// <param name="context">The gRPC call context providing information about the call.</param>
+    /// <returns>
+    /// A task representing the asynchronous operation, that indicates whether the submission was accepted 
+    /// and, if not, why it failed.
+    /// </returns>
     public override Task<Services.SubmissionResult> SubmitFood(Services.SubmitRequest request, ServerCallContext context)
     {
         var logicResult = mLogic.SubmitFood(request.Amount);
@@ -19,6 +31,15 @@ public class FarmService : Services.Farm.FarmBase
         return Task.FromResult(response);
     }
 
+    /// <summary>
+    /// Submits a specified amount of water to the farm for processing.
+    /// </summary>
+    /// <param name="request">The gRPC request message with the amount of water to submit.</param>
+    /// <param name="context">The gRPC call context providing information about the call.</param>
+    /// <returns>
+    /// A task representing the asynchronous operation, that indicates whether the submission was accepted
+    /// and, if not, why it failed.
+    /// </returns>
     public override Task<Services.SubmissionResult> SubmitWater(Services.SubmitRequest request, ServerCallContext context)
     {
         var logicResult = mLogic.SubmitWater(request.Amount);
